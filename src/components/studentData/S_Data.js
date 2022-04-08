7c89991509e61afb7e6fec6b17150c7b838a9fe7
Shahzaib <shahzaib191102@gmail.com>
2022-04-09 00:56:17 +0500

    changed data

--- src/components/studentData/S_Data.js
+++ src/components/studentData/S_Data.js
@@ -5,20 +5,20 @@ export const S_Data = [
         S_Id: 1,
         S_name: "Shahzaib",
         S_rollNo: "6512",
-        S_phone: +922399490974,
+        S_phone: +920000000000,
         S_course: "Web and Mobile App Development",
         S_batch: "4",
         S_age: "19",
-        S_address: "St # 2, House # 32, Sattelite Town"
+        S_address: "House# 233, St# 6, Bahria Town, Karachi"
     },
     {
         S_Id: 2,
         S_name: "Ali Usman",
         S_rollNo: "6503",
-        S_phone: +922320090974,
-        S_course: "Web and Mobile App Development",
+        S_phone: +920000000000,
+        S_course: "API Development",
         S_batch: "4",
         S_age: "20",
-        S_address: "St # 3, House # 2, Bahria Town"
+        S_address: "House# 32, St# 3, F-10, Islamabad "
     }
 ]
